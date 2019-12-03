27f0c95c81ef22aa3ce485177865f86e9676f432
Faizan_Office <mr.faizanhabib@gmail.com>
2019-12-03 17:36:17 +0500

    layout comment added in main activity

--- app/src/main/java/com/project/faizan/mynewsapp/MainActivity.kt
+++ app/src/main/java/com/project/faizan/mynewsapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     //this is main activity of project
+    //layout file of this is activity_main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
